60b9393b03106db3f575ef62cb886ec7604d0a5d
E5cu3l4DI* <escueladigitalcarvajal@gmail.com>
2021-03-28 16:25:14 -0500

    urlback

--- Zemoga-prueba-tecnica/plugins/axiosConfig.js
+++ Zemoga-prueba-tecnica/plugins/axiosConfig.js
@@ -1,6 +1,6 @@
 export default function ({ $axios }) {
   // Asigna el URL dependiendo de si está en producción o desarrollo
-  const url = 'http://localhost:4000/api/'
+  const url = 'https://zemoga-back.vercel.app/api/'
   // Se asignan los parámetros para todas las peticiones
   $axios.setBaseURL(url)
 }
